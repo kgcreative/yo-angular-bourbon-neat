14f9327d1a5fee92aaf28a98781a7d69ca55590c
Kevin Garcia <kevin@kgcreative.com>
2015-08-19 02:46:32 -0700

    gruntfile optimizations

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,9 +13,6 @@
 
 module.exports = function (grunt) {
 
-  // Load grunt tasks automatically
-  /* require('load-grunt-tasks')(grunt); */
-
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
@@ -26,14 +23,6 @@ module.exports = function (grunt) {
     cdnify: 'grunt-google-cdn'
   });
 
-  // Configurable paths
-  // disabled duplicate code from generator-bourbon-neat
-  /* var config = {
-      app: 'app',
-      dist: 'dist'
-  }; */
-
-  // enabled preferred code from generator-angular
   // Configurable paths for the application
   var appConfig = {
   app: require('./bower.json').appPath || 'app',
@@ -52,15 +41,8 @@ module.exports = function (grunt) {
         files: ['bower.json'],
         tasks: ['bowerInstall', 'wiredep']
       },
-      js: {
-        // disabled duplicate code from generator-bourbon-neat
-        // files: ['<%= config.app %>/scripts/{,*/}*.js'],
-        // tasks: ['jshint'],
-        //options: {
-        //    livereload: true
-        //}
 
-        // enabled preferred code from generator-angular
+      js: {
         files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
         tasks: ['jshint'],
         options: {
@@ -68,37 +50,30 @@ module.exports = function (grunt) {
         }
       },
 
-      // disabled duplicate code from generator-bourbon-neat
-      //jstest: {
-      //    files: ['test/spec/{,*/}*.js'],
-      //    tasks: ['test:watch']
-      //},
-
-      // enabled code from generator-angular
       jsTest: {
         files: ['test/spec/{,*/}*.js'],
         tasks: ['test:watch', 'newer:jshint:test', 'karma']
       },
+
       gruntfile: {
           files: ['Gruntfile.js']
       },
+
       sass: {
           files: ['<%= yeoman.app %>/sass/{,*/}*.{scss,sass}'],
           tasks: ['sass:server', 'autoprefixer']
       },
+
       styles: {
           files: ['<%= yeoman.app %>/styles/{,*/}*.css'],
           tasks: ['newer:copy:styles', 'autoprefixer']
       },
+
       livereload: {
           options: {
               livereload: '<%= connect.options.livereload %>'
         },
-        // files: [
-        //    '<%= config.app %>/{,*/}*.html',
-        //    '.tmp/styles/{,*/}*.css',
-        //    '<%= config.app %>/images/{,*/}*'
-        //]
+
         files: [
           '<%= yeoman.app %>/{,*/}*.html',
           '.tmp/styles/{,*/}*.css',
@@ -132,8 +107,6 @@ module.exports = function (grunt) {
                 ),
                 connect.static(appConfig.app)
               ];
-                //connect.static(config.app)
-             // ];
           }
         }
       },
@@ -149,7 +122,6 @@ module.exports = function (grunt) {
                 '/bower_components',
                  connect.static('./bower_components')
                ),
-              //connect.static(config.app)
               connect.static(appConfig.app)
             ];
           }
@@ -158,28 +130,11 @@ module.exports = function (grunt) {
       dist: {
         options: {
           open: true,
-          base: '<%= yeoman.dist %>'
-          // base: '<%= config.dist %>',
-          // livereload: false
+          base: '<%= yeoman.dist %>',
+          livereload: false
         }
       }
     },
-
-    // Empties folders to start fresh
-    // disabled duplicate code from generator-bourbon-neat
-    // clean: {
-    //    dist: {
-    //        files: [{
-    //            dot: true,
-    //            src: [
-    //                '.tmp',
-    //                '<%= config.dist %>/*',
-    //                '!<%= config.dist %>/.git*'
-    //            ]
-    //        }]
-    //    },
-    //    server: '.tmp'
-    //},
 
     // Make sure code styles are up to par and there are no obvious mistakes
     jshint: {
@@ -188,15 +143,9 @@ module.exports = function (grunt) {
         reporter: require('jshint-stylish')
       },
       all: {
-        //[]
-        //'Gruntfile.js',
-        //'<%= config.app %>/scripts/{,*/}*.js',
-        //'!<%= config.app %>/scripts/vendor/*',
-        //'test/spec/{,*/}*.js'
-        //]
         src: [
           'Gruntfile.js',
-          '<%= yeoman.app %>/scripts/{,*/}*.js', /* disabled temporarily to squash init bug */
+          '<%= yeoman.app %>/scripts/{,*/}*.js',
           '!<%= yeoman.app %>/scripts/vendor/*',
           'test/spec/{,*/}*.js'
         ]
@@ -213,7 +162,7 @@ module.exports = function (grunt) {
     mocha: {
       all: {
         options: {
-          run: false, //disables this run
+          run: true,
           urls: ['http://<%= connect.options.hostname %>:<%= connect.test.options.port %>/index.html']
         }
       }
@@ -541,7 +490,7 @@ module.exports = function (grunt) {
     karma: {
       unit: {
         configFile: 'test/karma.conf.js',
-        singleRun: false //temporary halt
+        singleRun: true
       }
     }
   });
@@ -573,9 +522,9 @@ module.exports = function (grunt) {
         'wiredep',
         'concurrent:test',
         'autoprefixer',
-        'connect:test'
-        //'mocha',
-        //'karma'
+        'connect:test',
+        // 'mocha',
+        'karma'
       ]);
     }
 
